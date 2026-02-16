85e1d0a86d8ed29dd16447df347cdcd3819af519
RAHUL BALAJI <rahul.balaji13289@gmail.com>
2026-02-16 23:26:45 +0530

    Update reverseBits.py

--- LeetCode/DCCFeb2026/reverseBits.py
+++ LeetCode/DCCFeb2026/reverseBits.py
@@ -1,1 +1,14 @@
+class Solution(object):
+    def reverseBits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = 0
+        
+        for i in range(32):
+            res = (res << 1) | (n & 1)
+            n >>= 1
 
+        return res
+        
