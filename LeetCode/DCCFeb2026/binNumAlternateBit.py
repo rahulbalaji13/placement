341329efc70fd97a055f20a15700186a2574797c
RAHUL BALAJI <rahul.balaji13289@gmail.com>
2026-02-18 18:04:30 +0530

    Create binNumAlternateBit.py

---
+++ LeetCode/DCCFeb2026/binNumAlternateBit.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def hasAlternatingBits(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        bits = bin(n)
+        return all(bits[i] != bits[i + 1] for i in range(len(bits) - 1))
