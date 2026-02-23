3fb659160b17b58c8ad10392baa709fe9198b0d5
RAHUL BALAJI <rahul.balaji13289@gmail.com>
2026-02-23 19:08:47 +0530

    Update checkIfAStrContainAllBinCodeOfSizek.py

--- LeetCode/DCCFeb2026/checkIfAStrContainAllBinCodeOfSizek.py
+++ LeetCode/DCCFeb2026/checkIfAStrContainAllBinCodeOfSizek.py
@@ -1,1 +1,14 @@
+class Solution(object):
+    def hasAllCodes(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: bool
+        """
+        seen = set()
+        
+        for i in range(len(s) - k + 1): #Sliding window
+            seen.add(s[i:i+k])
 
+        return len(seen) == 2 ** k
+        
